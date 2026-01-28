e7582cdf97dc88be36020807f9f78351dffc36e7
Harshit Halwai <harshithalwai001@gmail.com>
2026-01-28 22:49:33 +0530

    28-01-2026

---
+++ App_1/App_1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace App_1
+//{
+//    internal class Class1
+//    {
+//    }
+//}
+
+public class Emp
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+     
+    public void display()
+    {
+        Console.WriteLine("Id: " + Id);
+        Console.WriteLine("Name: " + Name);
+    }
+}
---
+++ App_1/App_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Emp obj=new Emp();
+            obj.Id=101;
+            obj.Name="John";
+            obj.display();
+            Console.WriteLine("Hello !");
+            Console.ReadKey();
+        }
+    }
+
+}
+
