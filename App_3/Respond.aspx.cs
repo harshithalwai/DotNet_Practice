e84b3d6a53e983cb253bf25231b7b3e483c90b71
Harshit Halwai <harshithalwai001@gmail.com>
2026-03-14 15:49:39 +0530

    Request.form | Request.QuesryString | PreviousPage.FindControl(idName)

--- App_3/Respond.aspx.cs
+++ App_3/Respond.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web.UI.WebControls;
 
 namespace App_3
 {
@@ -6,31 +7,41 @@ namespace App_3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ////For this method you need to set the form method to POST in Login.aspx
+
             //string username = Request.Form["txtUser"];
             //string email = Request.Form["txtEmail"];
             //string password = Request.Form["txtPass"];
 
-            string username = Request.QueryString["txtUser"];
-            string email = Request.QueryString["txtEmail"];
-            string password = Request.QueryString["txtPass"];
+            ////for this method you need to set the form method to GET in Login.aspx
 
-            if (!string.IsNullOrEmpty(username))
+            //string username = Request.QueryString["txtUser"];
+            //string email = Request.QueryString["txtEmail"];
+            //string password = Request.QueryString["txtPass"];
+
+
+
+            if (PreviousPage != null && PreviousPage.IsCrossPagePostBack)
             {
+                TextBox username = (TextBox)PreviousPage.FindControl("txtUser");
+                TextBox email = (TextBox)PreviousPage.FindControl("txtEmail");
+                TextBox password = (TextBox)PreviousPage.FindControl("txtPass");
+
                 Response.Write("<div style='font-family:Arial; padding:20px;'>");
 
-                Response.Write("<h2 style='color:#4A6CF7;'>Hello " + username + "</h2>");
+                Response.Write("<h2 style='color:#4A6CF7;'>Hello " + username.Text + "</h2>");
                 Response.Write("<p>We have received your contact details.</p>");
 
                 Response.Write("<hr/>");
 
-                Response.Write("<p><b>Email:</b> " + email + "</p>");
-                Response.Write("<p><b>Password:</b> " + password + "</p>");
+                Response.Write("<p><b>Email:</b> " + email.Text + "</p>");
+                Response.Write("<p><b>Password:</b> " + password.Text + "</p>");
 
                 Response.Write("</div>");
             }
             else
             {
-                Response.Write("<h3>No form data received.</h3>");
+                Response.Redirect("Login.aspx");
             }
         }
     }
