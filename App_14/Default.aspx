37bc3f9f43830dd6eb8a04e881d38199ab05420f
Harshit Halwai <harshithalwai001@gmail.com>
2026-03-22 22:21:55 +0530

    OracleCommandBuilder with oracle adapter

--- App_14/Default.aspx
+++ App_14/Default.aspx
@@ -1,15 +1,60 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="App_14.Default" %>
 
 <!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head runat="server">
-    <title></title>
+    <title>Student Form</title>
+    <style>
+        .row {
+            margin: 6px;
+        }
+
+        label {
+            width: 120px;
+            display: inline-block;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
+
         <div>
+
+            <div class="row">
+                <label>Student ID</label>
+                <asp:TextBox ID="txtStudentID" runat="server"></asp:TextBox>
+                <asp:Button ID="Button1" runat="server" Text="Load" OnClick="Button1_Click" />
+            </div>
+
+            <div class="row">
+                <label>Name</label>
+                <asp:TextBox ID="txtStudentName" runat="server"></asp:TextBox>
+            </div>
+
+            <div class="row">
+                <label>Gender</label>
+                <asp:DropDownList ID="ddlGender" runat="server">
+                    <asp:ListItem Text="Select Gender" Value=""></asp:ListItem>
+                    <asp:ListItem Text="Male" Value="Male"></asp:ListItem>
+                    <asp:ListItem Text="Female" Value="Female"></asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <div class="row">
+                <label>Total Marks</label>
+                <asp:TextBox ID="txtTotalMarks" runat="server"></asp:TextBox>
+            </div>
+
+            <div class="row">
+                <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
+            </div>
+
+            <div class="row">
+                <asp:Label ID="lblStatus" runat="server"></asp:Label>
+            </div>
+
         </div>
+
     </form>
 </body>
 </html>
--- App_14/Default.aspx.cs
+++ App_14/Default.aspx.cs
@@ -1,17 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Configuration;
+using System.Data;
 
 namespace App_14
 {
     public partial class Default : System.Web.UI.Page
     {
+        string cs = ConfigurationManager.ConnectionStrings["HRcon"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (OracleConnection con = new OracleConnection(cs))
+            {
+                string sqlQuery = "SELECT * FROM Students WHERE ID = :ID";
+
+                OracleDataAdapter da = new OracleDataAdapter(sqlQuery, con);
+                da.SelectCommand.Parameters.Add(":ID", OracleDbType.Int32).Value = Convert.ToInt32(txtStudentID.Text);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds, "Students");
+
+                ViewState["SQL_QUERY"] = sqlQuery;
+                ViewState["DATASET"] = ds;
+
+                if (ds.Tables["Students"].Rows.Count > 0)
+                {
+                    DataRow dr = ds.Tables["Students"].Rows[0];
+
+                    txtStudentName.Text = dr["NAME"].ToString();
+                    txtTotalMarks.Text = dr["MARKS"].ToString();
+                    ddlGender.SelectedValue = dr["GENDER"].ToString();
+                    lblStatus.Text = "";
+                }
+                else
+                {
+                    lblStatus.ForeColor = System.Drawing.Color.Red;
+                    lblStatus.Text = "No Student record with ID = " + txtStudentID.Text;
+                }
+            }
+        }
+
+        // UPDATE BUTTON
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            using (OracleConnection con = new OracleConnection(cs))
+            {
+                OracleDataAdapter da = new OracleDataAdapter((string)ViewState["SQL_QUERY"], con);
+
+                OracleCommandBuilder builder = new OracleCommandBuilder(da);
+
+                DataSet ds = (DataSet)ViewState["DATASET"];
+
+                if (ds.Tables["Students"].Rows.Count > 0)
+                {
+                    DataRow dr = ds.Tables["Students"].Rows[0];
+
+                    dr["NAME"] = txtStudentName.Text;
+                    dr["GENDER"] = ddlGender.SelectedValue;
+                    dr["MARKS"] = txtTotalMarks.Text;
+                }
+
+                da.Update(ds, "Students");
+
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                lblStatus.Text = "Record Updated Successfully";
+            }
+        }
+        //protected void Button2_Click(object sender, EventArgs e)
+        //{
+        //    using (OracleConnection con = new OracleConnection(cs))
+        //    {
+        //        string query = @"UPDATE Students 
+        //                         SET NAME = :Name,
+        //                             GENDER = :Gender,
+        //                             TOTALMARKS = :Marks
+        //                         WHERE ID = :ID";
+
+        //        OracleCommand cmd = new OracleCommand(query, con);
+
+        //        cmd.Parameters.Add(":Name", txtStudentName.Text);
+        //        cmd.Parameters.Add(":Gender", ddlGender.SelectedValue);
+        //        cmd.Parameters.Add(":Marks", txtTotalMarks.Text);
+        //        cmd.Parameters.Add(":ID", txtStudentID.Text);
+
+        //        con.Open();
+        //        int rows = cmd.ExecuteNonQuery();
+
+        //        if (rows > 0)
+        //        {
+        //            lblStatus.ForeColor = System.Drawing.Color.Green;
+        //            lblStatus.Text = "Record Updated Successfully";
+        //        }
+        //        else
+        //        {
+        //            lblStatus.ForeColor = System.Drawing.Color.Red;
+        //            lblStatus.Text = "Update Failed";
+        //        }
+        //    }
+        //}
     }
 }
