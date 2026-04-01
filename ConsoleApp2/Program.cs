a81efcf1464915253267aecdee90d2b536bf1256
Harshit Halwai <harshithalwai001@gmail.com>
2026-04-02 01:22:19 +0530

    02-04-26

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader file = new StreamReader(@"D:\Website\DotNet_Practice\ConsoleApp2\data.txt");
+            ////Console.WriteLine(file.ReadToEnd());
+            //Console.WriteLine(file.Read());
+            //file.Close();
+
+            int[] values = (int[])Enum.GetValues(typeof(Gender));
+            string[] names = Enum.GetNames(typeof(Gender));
+            foreach (int val in values)
+            {
+                Console.WriteLine(val);
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female
+    }
+}
