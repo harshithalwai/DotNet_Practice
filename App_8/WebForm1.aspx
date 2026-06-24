ef7468b0b574aa750f62508b23c2adb55eb2433d
Harshit Halwai <harshithalwai001@gmail.com>
2026-06-25 00:28:33 +0530

    command argument and events with buttons

---
+++ App_8/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="App_8.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--<asp:Button ID="Button1" runat="server" Text="Button" />--%>
+
+            <asp:Button ID="PrintButton" runat="server" Text="Print" OnCommand="CommandButton_Click" CommandName="Print" />
+            <asp:Button ID="DeletButton" runat="server" Text="Delete" OnCommand="CommandButton_Click" CommandName="Delete" />
+            <asp:Button ID="Top10Button" runat="server" Text="Show Top 10 Employees" OnCommand="CommandButton_Click"
+                CommandName="Show" CommandArgument="Top10" />
+            <asp:Button ID="Bottom10Button" runat="server" Text="Show Bottom 10 Employees" OnCommand="CommandButton_Click"
+                CommandName="Show" CommandArgument="Bottom10" />
+
+            <asp:Label ID="OutputLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ App_8/WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace App_8
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //Button1.Click += new EventHandler(Button1_Click);
+            //Button1.Command += new CommandEventHandler(Button1_Command);
+
+        }
+        //protected void Button1_Click(object sender, EventArgs e)
+        //{
+
+        //    Response.Write("Button Click Event <br/>");
+        //}
+
+        //protected void Button1_Command(object sender, CommandEventArgs e)
+        //{
+
+        //    Response.Write("Button Command Event <br/>");
+        //}
+
+
+        protected void CommandButton_Click(object sender, CommandEventArgs e)
+        {
+            switch (e.CommandName)
+            {
+                case "Print":
+                    OutputLabel.Text = "You clicked Print Button";
+                    break;
+
+                case "Delete":
+                    OutputLabel.Text = "You clicked Delete Button";
+                    break;
+
+                case "Show":
+                    if (e.CommandArgument.ToString() == "Top10")
+                    {
+                        OutputLabel.Text = "You clicked Show Top 10 Employees Button";
+                    }
+                    else
+                    {
+                        OutputLabel.Text = "You clicked Show Bottom 10 Employees Button";
+                    }
+                    break;
+            }
+        }
+    }
+}
