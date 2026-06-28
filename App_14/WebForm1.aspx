761dddae622a25bbddfa6b31a499dd6921cb32d2
Harshit Halwai <harshithalwai001@gmail.com>
2026-06-28 13:25:38 +0530

    Calender data

---
+++ App_14/WebForm1.aspx
@@ -0,0 +1,127 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="App_14.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calendar Demo</title>
+
+    <style>
+
+        body{
+            font-family:Segoe UI;
+            background:#f5f5f5;
+        }
+
+        .container{
+
+            width:450px;
+            margin:40px auto;
+            padding:20px;
+            background:white;
+            border-radius:10px;
+            box-shadow:0 0 10px lightgray;
+
+        }
+
+        .dateBox{
+
+            display:flex;
+            align-items:center;
+            gap:10px;
+
+        }
+
+        .calendar{
+
+            margin-top:10px;
+        }
+
+        .info{
+
+            margin-top:20px;
+            background:#eef6ff;
+            padding:10px;
+            border-radius:5px;
+
+        }
+
+    </style>
+
+</head>
+<body>
+
+<form id="form1" runat="server">
+
+<div class="container">
+
+<h2>Select Date</h2>
+
+<div class="dateBox">
+
+    <asp:TextBox
+        ID="TextBox1"
+        runat="server"
+        Width="250"
+        ReadOnly="true">
+    </asp:TextBox>
+
+    <asp:ImageButton
+        ID="ImageButton1"
+        runat="server"
+        Width="32"
+        Height="32"
+        ImageUrl="~/Images/calendar.png"
+        OnClick="ImageButton1_Click" />
+
+</div>
+
+<div class="calendar">
+
+<asp:Calendar
+
+    ID="Calendar1"
+    runat="server"
+
+    Visible="false"
+
+    OnSelectionChanged="Calendar1_SelectionChanged"
+
+    BackColor="White"
+    BorderColor="#999999"
+    CellPadding="5"
+    DayNameFormat="Shortest"
+    Font-Names="Segoe UI"
+    Font-Size="10pt"
+
+    NextPrevFormat="FullMonth"
+
+    TitleStyle-BackColor="#0078D7"
+    TitleStyle-ForeColor="White"
+
+    TodayDayStyle-BackColor="Yellow"
+
+    SelectedDayStyle-BackColor="#0078D7"
+    SelectedDayStyle-ForeColor="White"
+
+    WeekendDayStyle-BackColor="#F9E79F">
+
+</asp:Calendar>
+
+</div>
+
+<div class="info">
+
+<asp:Label
+ID="Label1"
+runat="server">
+</asp:Label>
+
+</div>
+
+</div>
+
+</form>
+
+</body>
+</html>
---
+++ App_14/WebForm1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace App_14
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //===========================
+        // Show / Hide Calendar
+        //===========================
+
+        protected void ImageButton1_Click(object sender, System.Web.UI.ImageClickEventArgs e)
+        {
+            Calendar1.Visible = !Calendar1.Visible;
+        }
+
+        //===========================
+        // Date Selected
+        //===========================
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            // Show selected date in textbox
+
+            TextBox1.Text = Calendar1.SelectedDate.ToString("dd/MM/yyyy");
+
+            // Hide Calendar
+
+            Calendar1.Visible = false;
+
+            // Show Calendar Properties
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("<h3>Calendar Properties</h3>");
+
+            sb.Append("Selected Date : " + Calendar1.SelectedDate + "<br/>");
+
+            sb.Append("Short Date : " + Calendar1.SelectedDate.ToShortDateString() + "<br/>");
+
+            sb.Append("Long Date : " + Calendar1.SelectedDate.ToLongDateString() + "<br/>");
+
+            sb.Append("Day : " + Calendar1.SelectedDate.Day + "<br/>");
+
+            sb.Append("Month : " + Calendar1.SelectedDate.Month + "<br/>");
+
+            sb.Append("Year : " + Calendar1.SelectedDate.Year + "<br/>");
+
+            sb.Append("Day Of Week : " + Calendar1.SelectedDate.DayOfWeek + "<br/>");
+
+            sb.Append("Day Of Year : " + Calendar1.SelectedDate.DayOfYear + "<br/>");
+
+            sb.Append("Visible Date : " + Calendar1.VisibleDate.ToShortDateString() + "<br/>");
+
+            sb.Append("Today's Date : " + Calendar1.TodaysDate.ToShortDateString() + "<br/>");
+
+            sb.Append("First Day Of Week : " + Calendar1.FirstDayOfWeek + "<br/>");
+
+            sb.Append("Selection Mode : " + Calendar1.SelectionMode + "<br/>");
+
+            sb.Append("Is Weekend : " +
+                (Calendar1.SelectedDate.DayOfWeek == DayOfWeek.Saturday ||
+                 Calendar1.SelectedDate.DayOfWeek == DayOfWeek.Sunday));
+
+            Label1.Text = sb.ToString();
+        }
+    }
+}
