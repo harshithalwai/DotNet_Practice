68ff9ed27cf7c77dafbd22ce1ceeaa8c4c88d760
Harshit Halwai <harshithalwai001@gmail.com>
2026-07-05 19:26:44 +0530

    Gmail sample

---
+++ App_19/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string senderEmail = "yourgmail@gmail.com";
+        string appPassword = "YOUR_APP_PASSWORD";
+
+        string receiverEmail = "xyz@gmail.com";
+
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress(senderEmail);
+        mail.To.Add(receiverEmail);
+
+        mail.Subject = "Test Mail from C#";
+
+        mail.Body = @"Hello,
+
+                    This mail is sent using Gmail SMTP.
+
+                    Regards,
+                    Harshit";
+
+        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+        smtp.Credentials = new NetworkCredential(senderEmail, appPassword);
+
+        smtp.EnableSsl = true;
+
+        try
+        {
+            smtp.Send(mail);
+            Console.WriteLine("Mail Sent Successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
